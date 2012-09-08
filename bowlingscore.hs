d409a009f7fa5f45570691f9fb0c9824eb92932a
Janne Rintanen <janne.rintanen@gmail.com>
2012-09-08 14:20:24 +0300

    refactor bowlingscore tests

--- bowlingscore.hs
+++ bowlingscore.hs
@@ -5,17 +5,13 @@ score (x:y:rest) | x+y == 10  = x + y + head rest + score rest
 score (x:rest) | x == 10 = x + (sum $ take 2 rest) + score rest
 score (x:y:rest) = x + y + score rest
 
-testbasic = score $ take 20 [1,1..]
--- 11 + 18 = 29
-testspare = score [1,9, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]
+testbasic = (2*10, score $ take 20 [1,1..])
+testspare = (11+18, score [1,9, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1])
+testlastframespare = (18+11, score [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,9,1])
+teststrike = (12+18, score [10, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1])
+testlastframestrike = (18+12, score [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10,1,1])
+testallstrikes = (300, score $ take 12 [10,10..])
 
--- 18 + 11 = 29
-testlastframespare = score [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,9,1]
+allresults = [testbasic, testspare, testlastframespare, teststrike, testlastframestrike, testallstrikes]
+testsok = all (\test -> fst test == snd test) allresults
 
-
--- 12 + 18 = 30
-teststrike = score [10, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]
--- 18 + 12
-testlastframestrike = score [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10,1,1]
--- 18 + 12
-testallstrikes = score $ take 12 [10,10..]
